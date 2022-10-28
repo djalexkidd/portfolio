b4cc0813aa42b47fc9382fcebafd61c883b7e531
djalexkidd <alexpyr@outlook.fr>
2022-10-28 09:30:16 +0200

    Page de connexion

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const gravatar = require('gravatar');
+const bcrypt = require ("bcrypt");
 
 const app = express();
+
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set('view engine', 'ejs');
+app.use(cors());
+app.use(bodyParser.json());
 
 const avatarUrl = gravatar.url('alexpyr@outlook.fr', {s: '200'}, true);
+
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
 
 const knex = require('knex')({
     client: 'mysql',
@@ -63,5 +72,96 @@ app.get("/contact", (req, res) => {
     });
   }
 );
+
+// Page de connexion
+app.get("/login", (req, res) => {
+    res.render("login.ejs", {
+        title: "Connexion",
+        currentPage3: false,
+        currentPage2: false,
+        currentPage1: false,
+        message: ""
+    });
+  }
+);
+
+app.post("/newproject", (req, res, next) => {
+    knex("projets").insert({
+       name: req.body.name,
+       image: req.body.image,
+       git_url: req.body.git_url
+    })
+    .then(users => {
+       res.json(users[0])
+    })
+    .catch(error => next(error))
+});
+
+app.post("/login", (req, res, next) => {
+    knex("users")
+    .where({username: req.body.username})
+    .first()
+    .then(user => {
+        console.log(user)
+       if(!user){
+        res.render("login.ejs", {
+            title: "Connexion",
+            currentPage3: false,
+            currentPage2: false,
+            currentPage1: false,
+            message: "Le nom d'utilisateur ou le mot de passe est incorrect."
+        });
+       }else{
+          return bcrypt
+          .compare(req.body.password, user.password_digest)
+          .then(isAuthenticated => {
+             if(!isAuthenticated){
+                res.render("login.ejs", {
+                    title: "Connexion",
+                    currentPage3: false,
+                    currentPage2: false,
+                    currentPage1: false,
+                    message: "Le nom d'utilisateur ou le mot de passe est incorrect."
+                });
+             }else{
+                return jwt.sign({user}, SECRET, {expiresIn: "1h"}, (error, token) => {
+                   res.status(200).json({token})
+                })
+             }
+          })
+       }
+    })
+});
+
+app.get("/verify", (req, res, next) => {
+    const token = req.headers.authorization
+    jwt.verify(token, SECRET, (error, decodedToken) => {
+       if(error){
+          res.status(401).json({
+             message: "Unauthorized Access!"
+          })
+       }else{
+          res.status(200).json({
+             id: decodedToken.id,
+             username: decodedToken.username
+          })
+       }
+    })
+});
+
+// app.post("/register", (request, response, next) => {
+//     bcrypt.hash(request.body.password, 10)
+//     .then(hashedPassword => {
+//        return knex("users").insert({
+//           username: request.body.username,
+//           password_digest: hashedPassword
+//        })
+//        .returning(["id", "username"])
+//        .then(users => {
+//           response.json(users[0])
+//        })
+//        .catch(error => next(error))
+//     })
+// });
 
 app.listen(3000, () => console.log("Server is running!"));
